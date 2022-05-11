4869168cc7800a743e32049bf10c63a9888619db
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-11 15:35:21 -0300

    repetição

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 
+import ListaAlunos from "./repeticao/ListaAlunos";
 import FamiliaMembro from "./components/basics/FamiliaMembro";
 import Familia from "./components/basics/Familia";
 import Card from "./components/layout/Card";
@@ -13,7 +14,12 @@ export default _ =>
     <div className="App">
         <h1>Fundamentos React</h1>
 
-        <Card titulo="#5 Componente com Filhos " color="#00C8F8">
+
+        <Card titulo="#06 Repetição" color="#00D6E6">
+                <ListaAlunos></ListaAlunos>
+            </Card>
+
+        <Card titulo="#05 Componente com Filhos" color="#00C8F8">
             <Familia sobrenome="Ramos">
                 <FamiliaMembro nome="Bruno" />
                 <FamiliaMembro nome="Joao" />
@@ -22,22 +28,22 @@ export default _ =>
         </Card>
 
         <div className="Cards">
-            <Card titulo="#4 Desafio Numeros Aleatório" color="#080">
+            <Card titulo="#04 Desafio Numeros Aleatório" color="#080">
                 <Aleatorio min={1} max={150} ></Aleatorio>
             </Card>
 
-            <Card titulo="#3 Fragmento">
+            <Card titulo="#03 Fragmento">
                 <Fragmento></Fragmento>
             </Card>
 
-            <Card titulo="#2 Com Parametro">
+            <Card titulo="#02 Com Parametro">
                 <ComParametro
                     titulo="Curso React e Redux"
                     subtitulo="Aprendendo react do zero + redux">
                 </ComParametro>
             </Card>
 
-            <Card titulo="#1 Primeiro Componente">
+            <Card titulo="#01 Primeiro Componente">
                 <Primeiro></Primeiro>
             </Card>
         </div>
---
+++ fundamentos-react/src/data/aluno.js
@@ -0,0 +1,10 @@
+export default [
+    {id: 1, nome: 'Ana', nota: 9.3},
+    {id: 2, nome: 'Bruno', nota: 3.4},
+    {id: 3, nome: 'Joao', nota: 5.1},
+    {id: 4, nome: 'Gustavo', nota: 5.5},
+    {id: 5, nome: 'Roberto', nota: 9.3},
+    {id: 6, nome: 'Pedro', nota: 8.9},
+    {id: 7, nome: 'Alberto', nota: 6.7},
+    {id: 8, nome: 'Joana', nota: 9.3},
+]
