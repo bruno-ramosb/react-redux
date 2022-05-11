3929f39dce3128bc30491da79e66628a5e35a77d
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-11 12:03:45 -0300

    Update index.js

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,8 +1,21 @@
+import './index.css'
 import ReactDom from 'react-dom'
 import React from 'react';
 
+import Primeiro from './components/basics/Primeiro';
+import ComParametro from './components/basics/ComParametro'
+import Fragmento from './components/basics/Fragmento';
+
 const tag = <strong>Seja bem vindo ao react !</strong>
 
+
 ReactDom.render(
-<div>{tag}</div>,
+<div id="app">
+    <Primeiro></Primeiro> 
+    <ComParametro 
+    titulo="Curso react" 
+    subtitulo="Aprendendo react do zero + redux">    
+    </ComParametro>
+    <Fragmento></Fragmento>
+</div>,
  document.getElementById('root'))
