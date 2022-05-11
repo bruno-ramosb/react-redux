f7c248ef69653dd6c93c7a030a0a19465b7be33b
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-11 12:03:18 -0300

    adicionando primeiro componente

---
+++ fundamentos-react/src/components/basics/Primeiro.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Primeiro() {
+    const msg = 'Seja bem vindo(a)!'
+    return (<div>
+        <h2 >React</h2>
+        <p>{msg}</p>
+        </div>)
+}
